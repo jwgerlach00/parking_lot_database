786543a48b409d220944dd9a72f3bad0453137b4
Soundarya Khanapur <soundarya.m.khanapur@gmail.com>
2023-11-15 11:19:53 -0500

    Transaction added to Permit and Citation class

--- parking_lot_database/src/parking_lot_database/Citation.java
+++ parking_lot_database/src/parking_lot_database/Citation.java
@@ -30,66 +30,97 @@ public class Citation {
     public boolean updateCitation(String citationNum, String newCitationDate, String newCitationTime,
                                   String newVehicleLicenseNum, String newVehicleModel, String newVehicleColor,
                                   String newParkingLotName, String newParkingLotAddress, String newCategory) {
-        return executeUpdateOperation(
-                "UPDATE Citations SET citationDate = ?, citationTime = ?, vehicleLicenseNum = ?, " +
-                        "vehicleModel = ?, vehicleColor = ?, parkingLotName = ?, parkingLotAddress = ?, " +
-                        "category = ? WHERE citationNum = ?",
-                newCitationDate, newCitationTime, newVehicleLicenseNum,
-                newVehicleModel, newVehicleColor, newParkingLotName, newParkingLotAddress, newCategory, citationNum);
-    }
-
-    public boolean detectParkingViolation(String licenseNum, String parkingLotName, String parkingLotAddress, String zoneID) {
-        boolean hasValidPermit = executeDetectViolationOperation(
-                "SELECT count(*) >= 1 AS validPermit " +
-                        "FROM PermitsAssignedVehicles NATURAL JOIN Permits " +
-                        "WHERE licenseNum=? AND parkingLotName=? AND parkingLotAddress=? AND zoneID=? " +
-                        "AND (expirationDate > CURRENT_DATE OR (expirationDate = CURRENT_DATE AND expirationTime > CURRENT_TIME))",
-                licenseNum, parkingLotName, parkingLotAddress, zoneID);
-
-        return hasValidPermit;
-    }
-
-    public boolean updateCitation(String citationNum, String newCitationDate, String newCitationTime,
-                                  String newVehicleLicenseNum, String newVehicleModel, String newVehicleColor,
-                                  String newParkingLotName, String newParkingLotAddress, String newCategory,
-                                  boolean newPaymentStatus, boolean newAppealRequested) {
-        return executeUpdateOperation(
-                "UPDATE Citations SET citationDate = ?, citationTime = ?, vehicleLicenseNum = ?, " +
-                        "vehicleModel = ?, vehicleColor = ?, parkingLotName = ?, parkingLotAddress = ?, " +
-                        "category = ?, paymentStatus = ?, appealRequested = ? WHERE citationNum = ?",
-                newCitationDate, newCitationTime, newVehicleLicenseNum,
-                newVehicleModel, newVehicleColor, newParkingLotName, newParkingLotAddress,
-                newCategory, newPaymentStatus, newAppealRequested, citationNum);
-    }
-
-    private boolean executeUpdateOperation(String sqlQuery, Object... params) {
         try (Connection connection = ParkingLotDB.initializeDatabase()) {
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
-                // Set parameters and execute SQL
-                for (int i = 0; i < params.length; i++) {
-                    preparedStatement.setObject(i + 1, params[i]);
-                }
+            connection.setAutoCommit(false); // Start transaction
 
-                int rowsAffected = preparedStatement.executeUpdate();
-                return rowsAffected > 0;
+            boolean result = executeUpdateOperation(
+                    connection,
+                    "UPDATE Citations SET citationDate = ?, citationTime = ?, vehicleLicenseNum = ?, " +
+                            "vehicleModel = ?, vehicleColor = ?, parkingLotName = ?, parkingLotAddress = ?, " +
+                            "category = ? WHERE citationNum = ?",
+                    newCitationDate, newCitationTime, newVehicleLicenseNum,
+                    newVehicleModel, newVehicleColor, newParkingLotName, newParkingLotAddress, newCategory, citationNum);
+
+            // If successful, commit the transaction
+            if (result) {
+                connection.commit();
+            } else {
+                connection.rollback(); // Rollback if there is an error
             }
+
+            return result;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
         }
     }
 
-    private boolean executeDetectViolationOperation(String sql, Object... params) {
+    public boolean detectParkingViolation(String licenseNum, String parkingLotName, String parkingLotAddress, String zoneID) {
         try (Connection connection = ParkingLotDB.initializeDatabase()) {
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-                // Set parameters and execute SQL
-                for (int i = 0; i < params.length; i++) {
-                    preparedStatement.setObject(i + 1, params[i]);
-                }
+            connection.setAutoCommit(false); // Start transaction
 
-                ResultSet resultSet = preparedStatement.executeQuery();
-                return resultSet.next() && resultSet.getBoolean("validPermit");
+            boolean hasValidPermit = executeDetectViolationOperation(
+                    connection,
+                    "SELECT count(*) >= 1 AS validPermit " +
+                            "FROM PermitsAssignedVehicles NATURAL JOIN Permits " +
+                            "WHERE licenseNum=? AND parkingLotName=? AND parkingLotAddress=? AND zoneID=? " +
+                            "AND (expirationDate > CURRENT_DATE OR (expirationDate = CURRENT_DATE AND expirationTime > CURRENT_TIME))",
+                    licenseNum, parkingLotName, parkingLotAddress, zoneID);
+
+            // If successful, commit the transaction
+            if (hasValidPermit) {
+                connection.commit();
+            } else {
+                connection.rollback(); // Rollback if there is an error
             }
+
+            return hasValidPermit;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean executeUpdateOperation(String sqlQuery, Object... params) {
+        try (Connection connection = ParkingLotDB.initializeDatabase();
+             PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+            // Set parameters and execute SQL
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean executeUpdateOperation(Connection connection, String sqlQuery, Object... params) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+            // Set parameters and execute SQL
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private boolean executeDetectViolationOperation(Connection connection, String sql, Object... params) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            // Set parameters and execute SQL
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next() && resultSet.getBoolean("validPermit");
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
--- parking_lot_database/src/parking_lot_database/Permit.java
+++ parking_lot_database/src/parking_lot_database/Permit.java
@@ -8,39 +8,89 @@ public class Permit {
     public boolean enterPermitInfo(int permitID, String startDate, String expirationDate, String expirationTime,
                                    String parkingLotName, String parkingLotAddress, String zoneID,
                                    String permitType, String spaceType) {
-        return executeUpdateOperation(
+        try (Connection connection = ParkingLotDB.initializeDatabase()) {
+            connection.setAutoCommit(false); // Start transaction
+
+            boolean result = executeUpdateOperation(
+                connection,
                 "INSERT INTO Permits VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
                 permitID, startDate, expirationDate, expirationTime,
                 parkingLotName, parkingLotAddress, zoneID, permitType, spaceType);
+
+            // If successful, commit the transaction
+            if (result) {
+                connection.commit();
+            } else {
+                connection.rollback(); // Rollback if there is an error
+            }
+
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public boolean updatePermitInfo(int permitID, String newPermitType, String newStartDate, String newExpirationDate,
                                     String newExpirationTime, String parkingLotName, String parkingLotAddress,
                                     String zoneID, String newSpaceType) {
-        return executeUpdateOperation(
+        try (Connection connection = ParkingLotDB.initializeDatabase()) {
+            connection.setAutoCommit(false); // Start transaction
+
+            boolean result = executeUpdateOperation(
+                connection,
                 "UPDATE Permits SET startDate=?, expirationDate=?, expirationTime=?, " +
                         "parkingLotName=?, parkingLotAddress=?, zoneID=?, permitType=?, spaceType=? " +
                         "WHERE permitID=?",
                 newStartDate, newExpirationDate, newExpirationTime,
                 parkingLotName, parkingLotAddress, zoneID, newPermitType, newSpaceType, permitID);
+
+            // If successful, commit the transaction
+            if (result) {
+                connection.commit();
+            } else {
+                connection.rollback(); // Rollback if there is an error
+            }
+
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public boolean deletePermitInfo(int permitID) {
-        return executeUpdateOperation("DELETE FROM Permits WHERE permitID=?", permitID);
+        try (Connection connection = ParkingLotDB.initializeDatabase()) {
+            connection.setAutoCommit(false); // Start transaction
+
+            boolean result = executeUpdateOperation(
+                connection,
+                "DELETE FROM Permits WHERE permitID=?",
+                permitID);
+
+            // If successful, commit the transaction
+            if (result) {
+                connection.commit();
+            } else {
+                connection.rollback(); // Rollback if there is an error
+            }
+
+            return result;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
-    private boolean executeUpdateOperation(String sqlQuery, Object... params) {
-        try {
-            Connection connection = ParkingLotDB.initializeDatabase();
-            try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
-                // Set parameters and execute SQL
-                for (int i = 0; i < params.length; i++) {
-                    preparedStatement.setObject(i + 1, params[i]);
-                }
-
-                int rowsAffected = preparedStatement.executeUpdate();
-                return rowsAffected > 0;
+    private boolean executeUpdateOperation(Connection connection, String sqlQuery, Object... params) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery)) {
+            // Set parameters and execute SQL
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
             }
+
+            int rowsAffected = preparedStatement.executeUpdate();
+            return rowsAffected > 0;
         } catch (SQLException e) {
             e.printStackTrace();
             return false;
