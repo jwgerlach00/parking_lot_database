4bebc7d4480c6ab2dfb5f17886f0a44c960f917e
Soundarya Khanapur <soundarya.m.khanapur@gmail.com>
2023-11-15 20:30:27 -0500

    code improvements

--- parking_lot_database/src/parking_lot_database/ReportGenerator.java
+++ parking_lot_database/src/parking_lot_database/ReportGenerator.java
@@ -3,80 +3,100 @@ package parking_lot_database;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class ReportGenerator {
-    
+
     public void generateTotalCitationsReport(String startDate, String endDate) {
         executeAndPrintQuery(
                 "SELECT parkingLotName, parkingLotAddress, COUNT(*) AS totalNumCitationsAllZones " +
-                "FROM Citations " +
-                "WHERE citationDate > ? AND citationDate < ? " +
-                "GROUP BY parkingLotName, parkingLotAddress;",
+                        "FROM Citations " +
+                        "WHERE citationDate > ? AND citationDate < ? " +
+                        "GROUP BY parkingLotName, parkingLotAddress;",
                 startDate, endDate);
     }
 
     public void listZonesForAllLots() {
         executeAndPrintQuery(
                 "SELECT parkingLotName, parkingLotAddress, zoneID " +
-                "FROM Zones;");
+                        "FROM Zones;");
     }
 
     public void countViolationCars() {
         executeAndPrintQuery(
                 "SELECT count(DISTINCT vehicleLicenseNum) AS NumCarsInViolation " +
-                "FROM Citations " +
-                "WHERE paymentStatus = false;");
+                        "FROM Citations " +
+                        "WHERE paid = false;");
     }
 
     public void countEmployeesPermitsByZone() {
         executeAndPrintQuery(
                 "SELECT zoneID, count(zoneID) AS numEmployees " +
-                "FROM Permits NATURAL JOIN (" +
-                "    SELECT * " +
-                "    FROM DriversObtainPermits JOIN Drivers ON id = driverID " +
-                "    WHERE status='E' " +
-                ") AS Employees " +
-                "GROUP BY zoneID;");
+                        "FROM Permits NATURAL JOIN (" +
+                        "    SELECT * " +
+                        "    FROM DriversObtainPermits JOIN Drivers ON id = driverID " +
+                        "    WHERE status='E' " +
+                        ") AS Employees " +
+                        "GROUP BY zoneID;");
     }
 
     public void getPermitInfoByDriverIdOrPhoneNum(String identifier) {
         executeAndPrintQuery(
                 "SELECT * " +
-                "FROM Permits NATURAL JOIN DriversObtainPermits " +
-                "WHERE driverID=?;", identifier);
+                        "FROM Permits NATURAL JOIN DriversObtainPermits " +
+                        "WHERE driverID=?;", identifier);
     }
 
     public void returnAvailableSpaceByTypeInLot(String spaceType, String parkingLotName, String parkingLotAddress) {
         executeAndPrintQuery(
                 "SELECT spaceNum " +
-                "FROM Spaces " +
-                "WHERE spaceType = ? AND availabilityStatus = true AND parkingLotName = ? AND parkingLotAddress = ? " +
-                "LIMIT 1;",
+                        "FROM Spaces " +
+                        "WHERE spaceType = ? AND availabilityStatus = true AND parkingLotName = ? AND parkingLotAddress = ? " +
+                        "LIMIT 1;",
                 spaceType, parkingLotName, parkingLotAddress);
     }
 
     private void executeAndPrintQuery(String query, Object... params) {
-        try {
-            Connection connection = ParkingLotDB.initializeDatabase();
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+        try (Connection connection = ParkingLotDB.initializeDatabase();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
 
             // Set parameters and execute SQL
             for (int i = 0; i < params.length; i++) {
                 preparedStatement.setObject(i + 1, params[i]);
             }
 
-            ResultSet resultSet = preparedStatement.executeQuery();
-            printResultSet(resultSet);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                printResultSet(resultSet);
+            }
 
         } catch (SQLException e) {
-            e.printStackTrace();
+            handleSQLException(e);
         }
     }
 
-    private void printResultSet(ResultSet resultSet) throws SQLException {
-        while (resultSet.next()) {
-            System.out.println(resultSet.getString(1)); // Assuming the query returns a single column
+    private void printResultSet(ResultSet resultSet) {
+        try {
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            if (resultSet.next()) {
+                do {
+                    for (int i = 1; i <= columnCount; i++) {
+                        System.out.print(metaData.getColumnName(i) + ": " + resultSet.getString(i) + " ");
+                    }
+                    System.out.println();
+                } while (resultSet.next());
+            } else {
+                System.out.println("No results found.");
+            }
+        } catch (SQLException e) {
+            handleSQLException(e);
         }
+    }
+
+
+    private void handleSQLException(SQLException e) {
+        e.printStackTrace(); 
     }
 }
